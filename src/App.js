e2cf58fbcb29901bf1c5c37732771788654b5f3a
Dan Pereda <dpereda@gmail.com>
2018-08-21 11:29:25 -0400

    Done with EP6

--- src/App.js
+++ src/App.js
@@ -5,15 +5,8 @@ import './App.css';
 
 
 class App extends Component {
-  state = {
-    thingy: true,
-
-  }
-
-  toggle= ()=>{
-    this.setState({
-      thingy: !this.state.thingy
-    })
+  submit= ()=>{
+    console.log(this.text.value)
   } 
    render() {
     return (
@@ -25,11 +18,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {this.state.thingy &&
-          <p>This should show and hide</p>
-        }
-        
-        <button onClick ={this.toggle} >Show /Hide</button>
+        <input type="text" ref={(input) => this.text = input}/>
+        <button onClick ={this.submit} >Show Value</button>
       </div>
     );
   }
