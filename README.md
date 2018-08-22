e7994a9c9f62179bdfc71eef8f695de0d7254ea9
Dan Pereda <dpereda@gmail.com>
2018-08-22 18:17:35 -0400

    Ep11 done

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is my learning of react
 
+Episode 11 Done
+
+Episode 10 Done
+
 Episode 9 Done
 
 Episode 8 Done 
--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Movie from './Movie'
 const movies = [
    {id: 1,
    title: 'Star Wars',
+   desc: 'space movie',
    },
    {id: 2,
    title: 'Deadpool',
@@ -19,9 +20,26 @@ const movies = [
   ];
 
 class App extends Component {
-  state ={
-    input: "Hello"
+  
+  state={
+    movies:{}
   }
+
+  async componentDidMount(){
+    try{
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=027913d6fdea8d4fa45952ac85185ad7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results
+      })
+    } catch(e){
+      console.log(e)
+    } 
+  }
+
+
+
 
   updateInput = (event) =>{
     console.log(event.target.value)
@@ -34,12 +52,14 @@ class App extends Component {
     console.log(this.text.value)
   } 
    render() {
+    console.log(this.state.movies);
     return (
+      
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-       {movies.map(movie => <Movie key={movie.id} movie ={movie}/>)}
+       {movies.map(movie => <Movie key={movie.id} movie ={movie} desc={movie.desc}/>)}
       </div>
     );
   }
