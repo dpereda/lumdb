87195794cfd3477e447dab049b91c837e68f263b
Dan Pereda <dpereda@gmail.com>
2018-08-23 10:35:53 -0400

    added MovieList and completed EP 15 and 16

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Movie from './Movie';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
 
-class App extends Component {
-   state={
-     movies: [],
-   }
 
-   async componentDidMount() {
-     try {
-       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=027913d6fdea8d4fa45952ac85185ad7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-       const movies = await res.json();
-       this.setState({
-         movies: movies.results,
-       });
-     } catch (e) {
-       console.log(e);
-     }
-   }
+import MoviesList from './MoviesList';
 
-   render() {
-     return (
-       <div className="App">
-         <header className="App-header">
-           <img src={logo} className="App-logo" alt="logo" />
-         </header>
-         {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-       </div>
-     );
-   }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route exact path="/:id" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
+
 
 export default App;
+
+
+const Test = ({ match }) => (
+  <h1>{match.params.id}</h1>
+);
